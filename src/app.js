16252a8793217cef5d791636200b9deca02889d8
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2022-11-04 12:23:44 -0300

    feat: configuração do controller de usuarios e arquivo de inicialização

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import Usuarios from './controllers/Usuarios.js'
+
+dotenv.config()
+
+const port = process.env.PORT || 3002
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
+
+app.use(express.json())
+app.use(cors())
+
+console.log(process.env.HOSTNAME)
+
+Usuarios.rotas(app)
