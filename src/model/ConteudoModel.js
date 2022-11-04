1727ab6dce79dc940ce56ea2a3b3205fcae6563a
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-04 16:34:24 -0300

    fix: corrigindo o model de conteudo

--- src/model/ConteudoModel.js
+++ src/model/ConteudoModel.js
@@ -1,10 +1,11 @@
 class ConteudoModel {
-  constructor (titulo, tipo, duracao, link) {
-    this.titulo = titulo
-    this.tipo = tipo
-    this.duracao = duracao
-    this.link = link
+  constructor(titulo, tipo, duracao, link, donoConteudo) {
+    this.titulo = titulo;
+    this.tipo = tipo;
+    this.duracao = duracao;
+    this.link = link;
+    this.donoConteudo = donoConteudo;
   }
 }
 
-export default ConteudoModel
+export default ConteudoModel;
--- src/model/UsuarioModel.js
+++ src/model/UsuarioModel.js
@@ -1,9 +1,9 @@
 class UsuarioModel {
-  constructor (nome, email, senha) {
-    this.nome = nome
-    this.email = email
-    this.senha = senha
+  constructor(nome, email, senha) {
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
   }
 }
 
-export default UsuarioModel
+export default UsuarioModel;
