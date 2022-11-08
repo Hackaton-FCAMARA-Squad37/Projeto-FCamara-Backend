1715bb6886ca70094fee8640252fc4332084366b
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-08 18:04:06 -0300

    fix: refatoração do conteudoController para usar a função de guard clause verificaSeExiste

--- src/controllers/conteudoController.js
+++ src/controllers/conteudoController.js
@@ -1,9 +1,13 @@
 import Conteudo from "../model/ConteudoModel.js";
+import { verificaSeExiste } from "../utils/verificaSeExiste.js";
 
 export const conteudoController = {
   async getAllConteudos(request, response) {
     try {
       const conteudos = await Conteudo.findAll();
+
+      verificaSeExiste(conteudos, "Nenhum conteúdo encontrado", 404);
+
       response.status(200).json(conteudos);
     } catch (error) {
       response.status(404).json(error.message);
@@ -13,6 +17,9 @@ export const conteudoController = {
   async getConteudoById(request, response) {
     try {
       const conteudo = await Conteudo.findByPk(request.params.id);
+
+      verificaSeExiste(conteudo, "Nenhum conteúdo encontrado com esse ID", 404);
+
       response.status(200).json(conteudo);
     } catch (error) {
       response.status(404).json(error.message);
@@ -21,7 +28,14 @@ export const conteudoController = {
 
   async postConteudo(request, response) {
     try {
-      await Conteudo.create(request.body);
+      const conteudoCriado = await Conteudo.create(request.body);
+
+      verificaSeExiste(
+        conteudoCriado,
+        "Conteudo não foi cadastrado com sucesso",
+        400
+      );
+
       response.status(201).json("Conteudo criado com sucesso!");
     } catch (error) {
       response.status(400).json(error.message);
@@ -30,6 +44,13 @@ export const conteudoController = {
   async putConteudo(request, response) {
     try {
       const conteudo = await Conteudo.findByPk(request.params.id);
+
+      verificaSeExiste(
+        conteudo,
+        "Não foi possível encontrar o conteúdo para atualização.",
+        400
+      );
+
       conteudo.titulo = request.body.titulo;
       conteudo.tipo = request.body.tipo;
       conteudo.duracao = request.body.duracao;
@@ -37,7 +58,14 @@ export const conteudoController = {
       conteudo.link = request.body.link;
       conteudo.donoConteudo = request.body.donoConteudo;
       conteudo.tags = request.body.tags;
-      await conteudo.save();
+      const resultado = await conteudo.save();
+
+      verificaSeExiste(
+        resultado,
+        "Conteúdo não foi atualizado com sucesso.",
+        400
+      );
+
       response.status(201).json("Conteudo atualizado com sucesso!");
     } catch (error) {
       response.status(400).json(error.message);
@@ -46,10 +74,21 @@ export const conteudoController = {
   async deleteConteudo(request, response) {
     try {
       const conteudo = await Conteudo.findByPk(request.params.id);
-      if (!conteudo) {
-        throw new Error("Conteudo não encontrado com esse Id");
-      }
-      await conteudo.destroy();
+
+      verificaSeExiste(
+        conteudo,
+        "Não foi possível encontrar o conteúdo para ser deletado.",
+        400
+      );
+
+      const resultado = await conteudo.destroy();
+
+      verificaSeExiste(
+        resultado,
+        "Conteúdo não foi deletado com sucesso.",
+        400
+      );
+
       response.status(200).json(conteudo);
     } catch (error) {
       response.status(404).json(error.message);
