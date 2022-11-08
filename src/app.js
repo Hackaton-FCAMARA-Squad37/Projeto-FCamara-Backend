dd4bf82b418a74af07f18f97c5a07b4ae1e83783
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-07 22:25:49 -0300

    fix: correção do arquivo database e app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import usuariosRouter from './routes/usuariosRoutes.js'
-import conteudosRouter from './routes/conteudosRoutes.js'
-import trilhasRouter from './routes/trilhasRoutes.js'
-import niveisRouter from './routes/niveisRoutes.js'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import usuariosRouter from "./routes/usuariosRoutes.js";
+import conteudosRouter from "./routes/conteudosRoutes.js";
+import trilhasRouter from "./routes/trilhasRoutes.js";
+import niveisRouter from "./routes/niveisRoutes.js";
+import sequelize from "./infra/Database.js";
 
-dotenv.config()
+await sequelize
+  .sync()
+  .then(() =>
+    console.log("Todos os modelos foram sincronizados! Banco de dados pronto!")
+  );
 
-const port = process.env.PORT || 3002
-const app = express()
+dotenv.config();
+
+const port = process.env.PORT || 3002;
+const app = express();
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
-})
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-console.log(process.env.HOSTNAME)
+console.log(process.env.HOSTNAME);
 
-app.use(usuariosRouter)
-app.use(conteudosRouter)
-app.use(trilhasRouter)
-app.use(niveisRouter)
+app.use(usuariosRouter);
+app.use(conteudosRouter);
+app.use(niveisRouter);
--- src/infra/Database.js
+++ src/infra/Database.js
@@ -1,9 +1,14 @@
-import sqlite3 from 'sqlite3'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import sqlite3 from "sqlite3";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import Sequelize from "sequelize";
 
-sqlite3.verbose()
-const filePath = dirname(fileURLToPath(import.meta.url)) + '/database.db'
-const Database = new sqlite3.Database(filePath)
+sqlite3.verbose();
+const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db";
 
-export default Database
+const sequelize = new Sequelize("orange-evolution", "user", "password", {
+  dialect: "sqlite",
+  storage: filePath,
+});
+
+export default sequelize;
