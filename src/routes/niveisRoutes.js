bf1ae525c54af883bd4e233097a7adfadd2513d8
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-07 23:17:49 -0300

    fix: corrigindo e refatorando niveisRoutes

--- src/routes/niveisRoutes.js
+++ src/routes/niveisRoutes.js
@@ -1,20 +1,27 @@
-import express from 'express'
-import Niveis from '../controllers/Niveis.js'
-import validation from '../middlewares/validationMiddleware.js'
-import nivelSchema from '../validations/nivelValidation.js'
+import express from "express";
+import { nivelController } from "../controllers/nivelController.js";
+import validation from "../middlewares/validationMiddleware.js";
+import nivelSchema from "../validations/nivelValidation.js";
 
-const niveisRouter = express.Router()
+const niveisRouter = express.Router();
 
 // GET /Niveis
-niveisRouter.get('/niveis', Niveis.getAllNiveis)
+niveisRouter.get("/niveis", nivelController.getAllNiveis);
 
 // GET /Niveis/:id
-niveisRouter.get('/niveis/:id', Niveis.getNiveisById)
+niveisRouter.get("/niveis/:id", nivelController.getNiveisById);
+
+// GET /Niveis/:id/temas
+niveisRouter.get("/niveis/:id/temas", nivelController.getAllTemas);
 
 // POST /Niveis
-niveisRouter.post('/niveis', validation(nivelSchema), Niveis.postNivel)
+niveisRouter.post(
+  "/niveis",
+  validation(nivelSchema),
+  nivelController.postNivel
+);
 
 // DELETE "/Niveis/:id"
-niveisRouter.delete('/niveis/:id', Niveis.deleteNivel)
+niveisRouter.delete("/niveis/:id", nivelController.deleteNivel);
 
-export default niveisRouter
+export default niveisRouter;
