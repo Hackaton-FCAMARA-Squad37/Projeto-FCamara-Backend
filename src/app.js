adbfdf6f8ac57b012354bd44d30b70e1b9b0aae2
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-11 00:56:56 -0300

    feat: update cors app.jg

--- src/app.js
+++ src/app.js
@@ -23,6 +23,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-Width, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 console.log(process.env.HOSTNAME);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
